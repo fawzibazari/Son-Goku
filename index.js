d589b2676349ed9df8b2324c3a586a3b0c3a8e17
fawzibazari <fawzi.bazari2@gmail.com>
2023-03-16 22:41:36 +0100

    i think it's working i have to go running

--- index.js
+++ index.js
@@ -7,8 +7,13 @@
  *  2. INTEGER_ARRAY dist
  */
 
-const food = [2, 4, 5, 2];
-const dist = [4, 3, 1, 3];
+// const food = [2, 4, 5, 2];
+// const food_copie = [2, 4, 5, 2];
+// const dist = [4, 3, 1, 3];
+
+const food = [3, 2, 5, 4];
+const food_copie = [3, 2, 5, 4];
+const dist = [2, 3, 4, 2];
 
 function optimalPosition(foods, dists) {
   const n = foods.length;
@@ -20,23 +25,30 @@ function optimalPosition(foods, dists) {
     const food = foods[index];
     const dist = dists[index];
     console.log({ food: food });
-    const newArray = index == 0 ? dists : arraySwitcher(dists);
-    for (let j = 0; j < newArray.length; j++) {
-      console.log(foods[j]);
+    const newDistArray = index == 0 ? dists : distArraySwitcher(dists);
+    const newFoodArray = (index == 0 ? foods : foodArraySwitcher(food_copie));
+    for (let j = 0; j < newDistArray.length; j++) {
+
+      // console.log(foods[j + 1] != undefined ? newFoodArray[j + 1]: 0);
       const element = foods[index];
       const element2 = dists[j];
-      // console.log(
-      //   `${final_res ?? element} - ${element2 ?? dists[0]} + ${
-      //     foods[j + 1] ?? 0
-      //   }`
-      // );
+      console.log(
+        `${final_res ?? element} - ${element2 ?? dists[0]} + ${
+          foods[j + 1] != undefined ? newFoodArray[j + 1]: 0
+        } = `
+      );
       final_res =
-        (final_res ?? element) - (element2 ?? dists[0]) + (foods[j + 1] ?? 0);
+        (final_res ?? element) - (element2 ?? dists[0]) + (foods[j + 1] != undefined ? newFoodArray[j + 1]: 0);
+        console.log(final_res);
     }
   }
 }
 
-function arraySwitcher(array) {
+function distArraySwitcher(array) {
+  array.push(array.shift(array));
+  return array;
+}
+function foodArraySwitcher(array) {
   array.push(array.shift(array));
   return array;
 }
