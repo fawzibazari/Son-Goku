b6164c6eb85c935355ec0c77e322dc7cf7b93a74
fawzibazari <fawzi.bazari2@gmail.com>
2023-03-19 19:56:42 +0100

    cleanned the code

--- index.js
+++ index.js
@@ -7,16 +7,19 @@
  *  2. INTEGER_ARRAY dist
  */
 
-// const food = [2, 4, 5, 2];
-// const food_copie = [2, 4, 5, 2];
-// const dist = [4, 3, 1, 3];
+// const food = [3, 2, 5, 4];
+// const dist = [2, 3, 4, 2];
 
-const food = [3, 2, 5, 4];
-const food_copie = [3, 2, 5, 4];
-const dist = [2, 3, 4, 2];
+const food = [2, 4, 5, 2];
+const dist = [4, 3, 1, 3];
+
+// const food = [8, 4, 1, 9];
+// const dist = [10, 9, 3, 5];
 
 function optimalPosition(foods, dists) {
   const n = foods.length;
+  const food_copie = [...foods];
+  const finalArray = [];
   console.log(foods);
   console.log(dists);
   let res = 0;
@@ -24,32 +27,44 @@ function optimalPosition(foods, dists) {
     let final_res = null;
     const food = foods[index];
     const dist = dists[index];
-    console.log({ food: food });
-    const newDistArray = index == 0 ? dists : distArraySwitcher(dists);
-    const newFoodArray = (index == 0 ? foods : foodArraySwitcher(food_copie));
+    // console.log({ food: food, index: index });
+    const newDistArray = index == 0 ? dists : arraySwitcher(dists);
+    const newFoodArray = index == 0 ? foods : arraySwitcher(food_copie);
     for (let j = 0; j < newDistArray.length; j++) {
-
-      // console.log(foods[j + 1] != undefined ? newFoodArray[j + 1]: 0);
       const element = foods[index];
       const element2 = dists[j];
-      console.log(
-        `${final_res ?? element} - ${element2 ?? dists[0]} + ${
-          foods[j + 1] != undefined ? newFoodArray[j + 1]: 0
-        } = `
-      );
-      final_res =
-        (final_res ?? element) - (element2 ?? dists[0]) + (foods[j + 1] != undefined ? newFoodArray[j + 1]: 0);
-        console.log(final_res);
+      if ((final_res ?? element) - (element2 ?? dists[0]) < 0) {
+        // console.log("negative");
+        break;
+      } else {
+        // console.log(
+        //   `${final_res ?? element} - ${element2 ?? dists[0]} + ${
+        //     newFoodArray[j + 1] ?? 0
+        //   } = `
+        // );
+        final_res =
+          (final_res ?? element) -
+          (element2 ?? dists[0]) +
+          (newFoodArray[j + 1] ?? 0);
+        // console.log(final_res);
+        if (j === newDistArray.length - 1) {
+          finalArray.push(index);
+        }
+      }
     }
   }
+  const result = finalArray.length == 0 ? -1 : Math.min(...finalArray);
+  console.log(
+    result == -1
+      ? "There is Zero path where Son Goku can finish his travel 😓"
+      : `The optimal position for Son Goku to finish hes travel is: ${result}`
+  );
+  return result;
 }
 
-function distArraySwitcher(array) {
+function arraySwitcher(array) {
   array.push(array.shift(array));
   return array;
 }
-function foodArraySwitcher(array) {
-  array.push(array.shift(array));
-  return array;
-}
+
 optimalPosition(food, dist);
